be4170a74d56537302be784b2ce368d62242baf8
numoniam <savanghori1203@gmail.com>
2023-04-22 13:55:32 +0530

    added

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+import mongose from 'mongoose'
+import colors from 'colors'
+
+
+// a mongoose stuf (mongoose.connect ....) return always a promise
+const connectDB = async () => {
+    try {
+        console.log()
+        const conn = await mongose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    } catch (error) {
+        console.error(`Error: ${error.message}`.red.underline.bold)
+        process.exit(1)
+    }
+}
+
+export default connectDB
